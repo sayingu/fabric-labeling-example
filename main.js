f3cc6eccbfc39069249247ba3cfea7b0fbd5c1e2
R11801 <R11801@LCNC08V0627.corp.lgcns.com>
2019-04-11 16:10:11 +0900

    etc.c.c.c.

--- main.js
+++ main.js
@@ -11,4 +11,4 @@ app.use('/', router);
 app.use('/', express.static('./'));
 app.listen(process.env.port || 3000);
 
-console.log('Running at Port 3000');
+console.log(`Running at Port ${process.env.port || 3000}`);
